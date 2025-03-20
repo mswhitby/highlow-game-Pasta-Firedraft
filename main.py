16637992e4a99b4e1420a17760687da8cc810b1f
Pasta-Firedraft <thomasa6847@student.judsonisd.org>
2025-03-20 18:55:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import random
 
+HighLow = random.randint(1,100)
+
+def luck_of_the_draw(input):
+    while input != HighLow:
+        x = input("Your Guess? ")
+        y = int(x)
+        if y < HighLow:
+            print("Higher")
+        if y > HighLow:
+            print("Lower")
+        if y == HighLow:
+            return print("Correct!")
+            
+            
+luck_of_the_draw(input)
